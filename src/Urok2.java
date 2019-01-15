fdc7ded661cac68a64d73eca112e5b58e781d86d
Bair <pinhaev17@gmail.com>
2019-01-15 20:43:31 +0800

    2

--- src/Urok2.java
+++ src/Urok2.java
@@ -4,20 +4,29 @@ import java.util.Random;
 public class Urok2 {
     Random random = new Random();
     static Integer[] a = new Integer[10];
+    static Integer[] b = new Integer[8];
     static Urok2 u = new Urok2();
     public static void main(String[] args) {
         for(int i = 0; i < a.length; i++){
             a[i] = u.zapoln();
         }
         for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+            System.out.print(" " + a[i]);
         }
+        System.out.println(" ");
         for (int i = 0; i < a.length; i++) {
             a[i] = u.zamena(a[i]);
         }
-        System.out.println("zamena");
         for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+            System.out.print(" " + a[i]);
+        }
+        System.out.println(" ");
+        b[0] = 0;
+        for(int i = 1; i < b.length; i++){
+            b[i] = b[i - 1] + 3;
+        }
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(" " + b[i]);
         }
     }
     public Integer zapoln() {
