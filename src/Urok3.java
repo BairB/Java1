289bdd61247d39b9fc4897c43cac3f07fe387fbb
Bair <pinhaev17@gmail.com>
2019-01-17 21:17:10 +0800

    euflfkrf

---
+++ src/Urok3.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Urok3 {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Urok3 u = new Urok3();
+        int x, p;
+        p = 3;
+        x = random.nextInt(10);
+        System.out.println(x);
+        u.ugadai(x,p);
+    }
+    public void ugadai(int x, int p) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        while (i < p) {
+            int z = scanner.nextInt();
+            if (x == z) {
+                System.out.println("ugadal");
+                break;
+            }
+            else {
+                System.out.println("ne ugadal");
+                i++;
+            }
+        }
+    }
+}
