b9bad05c323b63a1c3acb7da67f8562dfc26def5
Bair <pinhaev17@gmail.com>
2019-01-19 17:49:17 +0800

    333

--- src/Urok3.java
+++ src/Urok3.java
@@ -1,16 +1,18 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Urok3 {
-
+    Random random1 = new Random();
     public static void main(String[] args) {
         Random random = new Random();
         Urok3 u = new Urok3();
         int x, p;
         p = 3;
         x = random.nextInt(10);
-        System.out.println(x);
-        u.ugadai(x,p);
+        /*System.out.println(x);
+        u.ugadai(x,p);*/
+        u.slovo();
     }
     public void ugadai(int x, int p) {
         Scanner scanner = new Scanner(System.in);
@@ -35,5 +37,26 @@ public class Urok3 {
             System.out.print("Repeat the game?\n[1 - yes / 0 - no]: ");
         }
         while (scanner.next().equals("1"));
+    }
+    public void slovo(){
+        Scanner sc = new Scanner(System.in);
+        String x;
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
+                "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
+                "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        System.out.println(Arrays.toString(words));
+        String z = words[random1.nextInt(words.length)];
+        System.out.println(z);
+        do {
+            System.out.print("Guess the word: ");
+            x = sc.next();
+            for (int i = 0; i < 15; i++)
+                if (i < z.length() && i < x.length() &&
+                        z.charAt(i) == x.charAt(i))
+                    System.out.print(z.charAt(i));
+                else
+                    System.out.print((z.equals(x))? "" : "#");
+            System.out.println();
+        } while (!z.equals(x));
     }
 }
