16330b17e13b592dd54650651ed16def3d868331
Bair <pinhaev17@gmail.com>
2019-01-21 20:46:34 +0800

    jhgjfhjhg

--- src/Urok5.java
+++ src/Urok5.java
@@ -4,11 +4,11 @@ public class Urok5 {
     public static void main(String[] args) {
         Random random = new Random();
         Person[] persArr = new Person[5];
-        persArr[0] = new Person("Ivan", "vrach", "vanya@mail.ru", 112233, 44, 500);
-        persArr[1] = new Person("Petr", "ment", "petr@mail.ru", 223344, 50, 600);
+        persArr[0] = new Person("Ivan", "vrach", "vanya@mail.ru", 112233, 46, 500);
+        persArr[1] = new Person("Petr", "ment", "petr@mail.ru", 223344, 30, 600);
         persArr[2] = new Person("Vasya", "buh", "vasya@mail.ru", 112233, 44, 700);
-        persArr[3] = new Person("Kolya", "manager", "kolya@mail.ru", 112233, 44, 800);
-        persArr[4] = new Person("Dima", "program", "dima@mail.ru", 112233, 44, 900);
+        persArr[3] = new Person("Kolya", "manager", "kolya@mail.ru", 112233, 24, 800);
+        persArr[4] = new Person("Dima", "program", "dima@mail.ru", 112233, 34, 900);
 
         for(int i = 0; i < persArr.length; i++) {
         }
